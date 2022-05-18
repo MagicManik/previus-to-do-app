6b3730cce659af3f188536f3134ac5e9aae6a3d4
MagicManik <manikislam1952@gmail.com>
2022-05-19 04:51:39 +0600

    complite button added

--- src/Pages/Home/Task/Task.js
+++ src/Pages/Home/Task/Task.js
@@ -3,12 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Task.css'
 
-const Task = ({ task }) => {
+const Task = ({ task, lineThrough }) => {
     const { name, description } = task;
+    console.log(lineThrough)
     return (
         <div className='task-body'>
-            <h5>{name}</h5>
-            <p>{description}</p>
+            <h5 className={lineThrough ? 'linethrough' : ''}>{name}</h5>
+            <p className={lineThrough ? 'linethrough' : ''}>{description}</p>
             <div className='d-flex flex-row-reverse px-3'>
                 <button className='task-delete-btn'><FontAwesomeIcon icon={faTrash} ></FontAwesomeIcon></button>
             </div>
--- src/Pages/Home/Tasks/Tasks.js
+++ src/Pages/Home/Tasks/Tasks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTasks from '../../../hooks/useTasks';
 import Task from '../Task/Task';
 import './Tasks.css'
@@ -6,19 +6,28 @@ import './Tasks.css'
 const Tasks = () => {
     const [tasks, setTasks] = useTasks();
     // console.log(tasks);
+    const [lineThrough, setLineThrough] = useState(false);
 
-
+    const handleTaskComplite = () => {
+        setLineThrough(true);
+        return alert('Task complite')
+    }
 
     return (
-        <section className='tasks-container'>
-            {
-                tasks.map(task =>
-                    <Task
-                        key={task._id}
-                        task={task}
-                    >
-                    </Task>)
-            }
+        <section className='tasks-bg'>
+            <div className='tasks-container'>
+                {
+                    tasks.map(task =>
+                        <Task
+                            key={task._id}
+                            task={task}
+                            lineThrough={lineThrough}
+                        >
+                        </Task>)
+                }
+            </div>
+
+            <button onClick={handleTaskComplite} className='task-complite-btn'>Task Complite</button>
         </section>
     );
 };
